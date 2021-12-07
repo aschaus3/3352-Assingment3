6bb4975979fe5bd0d5446085a81e020e60e74a4d
Andrew Schaus <andrewschaus3@gmail.com>
2021-12-07 14:43:40 -0500

    Added a couple basic functions

--- src/Controller/LessonsController.java
+++ src/Controller/LessonsController.java
@@ -1,4 +1,25 @@
 package Controller;
 
-public class LessonsController {
+public class LessonsController
+{
+    // Giving the controller access to the model and view
+    Model.Lessons model;
+    View.LessonsPage view;
+
+    public LessonsController(Model.Lessons model, View.LessonsPage view)
+    {
+        this.model = model;
+        this.view = view;
+        this.view.setController(this);
+
+        // Allows controller to display the view
+        view.show();
+    }
+
+    // Updates the course number, Called in View
+    public void updateCourseNum(String newNum)
+    {
+        //Method to update the course number
+        model.setCourseNum(newNum);
+    }
 }
--- src/View/LessonsPage.java
+++ src/View/LessonsPage.java
@@ -1,4 +1,44 @@
 package View;
 
-public class LessonsPage {
+public class LessonsPage
+{
+    // Linking the View to the Model and Controller
+    private Model.Lessons model;
+    private Controller.LessonsController controller;
+
+    public LessonsPage(Model.Lessons model, Controller.LessonsController controller)
+    {
+        this.setModel(model);
+        this.setController(controller);
+    }
+
+    // Get user input from the view, update course number with it
+    public void onCourseNumUpdate()
+    {
+        //Collect data from user input
+        String userInput = "new number";
+        this.controller.updateCourseNum(userInput);
+    }
+
+    // For actually displaying the UI
+    public void show()
+    {
+        //Would display or refresh the page
+    }
+
+    public Model.Lessons getModel() {
+        return model;
+    }
+
+    public void setModel(Model.Lessons model) {
+        this.model = model;
+    }
+
+    public Controller.LessonsController getController() {
+        return controller;
+    }
+
+    public void setController(Controller.LessonsController controller) {
+        this.controller = controller;
+    }
 }
